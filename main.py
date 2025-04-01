6801bcb4c0d7df18ac8724bc2f43e5c4317bad59
henryhazelton <henryhazelton00@gmail.com>
2025-04-01 06:09:47 +0100

    Added the choosing functionality to the project in the main file

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ def main():
         choice = input("Enter your choice: ")
         print(f"You have picked option: {choice}")
 
+        if choice == '1':
+            addToInventory.add_item()
+        elif choice == '2':
+            removeFromInventory.remove_item()
+        elif choice == '3':
+            editInventory.edit_item()
+        elif choice == '4':
+            viewInventory.view_inventory()
+        elif choice == '5':
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid choice, please try again")
+
+
 
 if __name__ == "__main__":
     main()
